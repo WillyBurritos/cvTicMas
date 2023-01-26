deecf0f26854e0a71ea36a2199042955c7362039
Maxi Benavides <maxidoza@outlook.com>
2023-01-26 20:26:35 -0300

    cambios 26/01

--- cv.js
+++ cv.js
@@ -1,12 +1,11 @@
 document.getElementById('botón').onclick = function enviarCorreo(insTextoComp) {
-    var sEmail = "williamsonbu@outlook" ;
-    if(sEmail != null) {
+    var sEmail = "williamsonbu@outlook";
+    if (sEmail != null) {
         var sLink = "mailto:" + escape(sEmail)
-         + "?subject=" + escape("Me contacto por cv online")
-         + "&body=" + insTextoComp;
+            + "?subject=" + escape("Me contacto por cv online")
+            + "&body=" + insTextoComp;
         window.location.href = sLink;
     }
-    
 }
 
 const btnSwitch = document.querySelector('#switch');
@@ -14,7 +13,6 @@ const btnSwitch = document.querySelector('#switch');
 btnSwitch.addEventListener('click', () => {
     document.body.classList.toggle('dark');
     btnSwitch.classList.toggle('active');
-    
 
 });
 
